a809cc4c8bdbc86612d397a80ecb8e9415528c5b
edelkas <edlucasma@gmail.com>
2020-05-12 07:02:53 +0200

    parsing forums, parsing more thread info, parsing all pages of a thread

--- tapascrap.rb
+++ tapascrap.rb
@@ -1,15 +1,22 @@
 require 'open-uri'
 require 'nokogiri'
 
-TOPIC_START = 1
-TOPIC_END = 24462
+THREAD_START = 1
+THREAD_END = 24462
+FORUM_START = 1
+FORUM_END = 66
+POSTS_PER_PAGE = 10
 
-def url_thread(t, s)
+def url_thread(t, s = 0)
   URI("https://www.tapatalk.com/groups/metanetfr/viewtopic.php?t=#{t}&start=#{s}")
 end
 
 def url_member(id)
   URI("https://www.tapatalk.com/groups/metanetfr/memberlist.php?mode=viewprofile&u=#{id}")
+end
+
+def url_forum(id)
+  URI("https://www.tapatalk.com/groups/metanetfr/viewforum.php?f=#{id}")
 end
 
 def download_thread(t, s)
@@ -20,6 +27,48 @@ def download_member(id)
   Nokogiri::HTML(open(url_member(id)))
 end
 
+def download_forum(id)
+  Nokogiri::HTML(open(url_forum(id)))
+end
+
+def parse_posts(t, s)
+  doc = download_thread(t, s)
+  posts = doc.at('div[class="viewtopic_wrapper topic_data_for_js"]')
+             .search('div[class="postbody"]')
+             .map{ |p|
+               content = p.at('div[class="content"]')
+               content.search('i[class="hide"]').last.remove
+               {
+                 t: t,
+                 id:       p['id'][/\d+/].to_i,
+                 user:     p.parent.at('dl')['data-uid'].to_i,
+                 username: p.parent.at('dl').at('a[itemprop="name"]').content,
+                 date:     p.at('time')['datetime'],
+                 content:  content.inner_html
+               }
+             }
+end
+
+def parse_thread(t)
+  doc = download_thread(t)
+  atts = {
+    id: t,
+    name: doc.at('h1[itemprop="headline"]').content.to_s,
+    user_id: doc.at('h1[itemprop="headline"]').parent.at('dl')['data-uid'],
+    username: doc.at('h1[itemprop="headline"]').parent.at('a[class="username"]').content.to_s,
+    posts: doc.at('div[class="pagination"]').content[/\d+/i].to_i,
+    pages: doc.at('div[class="pagination"]').search('a[class="button"]').last.content.to_i
+  }
+  (1..atts[:pages]).each{ |page| parse_posts(t, POSTS_PER_PAGE * (s - 1)) }
+end
+
+def parse_threads
+  (THREAD_START..THREAD_END).each{ |t| parse_thread(t) }
+end
+
+# Note: Since we don't know the member list, we should first parse all posts,
+# and create all members based on the posts (we won't be able to find members
+# who didn't post). After that, we loop through them executing this method.
 def parse_member(id)
   doc = download_member(id)
   atts = {
@@ -36,7 +85,7 @@ def parse_member(id)
   atts[:active] = field.at('span[class="timespan"]')['title'] if !field.nil?
   field = fields.select{ |f| f.children[0].content.downcase == "total posts" }[0]
   atts[:posts] = field.at('a').content.strip.to_i if !field.nil?
-  
+
   field = fields.select{ |f| f.children[0].content.downcase == "groups" }[0]
   if !field.nil?
     groups = field.search('option').map{ |g| [g['value'].to_i, g.content] }.to_h
@@ -46,19 +95,13 @@ def parse_member(id)
   atts[:signature] = doc.at('div[class="signature standalone"]').inner_html rescue nil
 end
 
-def parse_thread(t, s)
-  doc = download_thread(t, s)
-  posts = doc.at('div[class="viewtopic_wrapper topic_data_for_js"]')
-             .search('div[class="postbody"]')
-             .map{ |p|
-               content = p.at('div[class="content"]')
-               content.search('i[class="hide"]').last.remove
-               {
-                 id:       p['id'][/\d+/].to_i,
-                 user:     p.parent.at('dl')['data-uid'].to_i,
-                 username: p.parent.at('dl').at('a[itemprop="name"]').content,
-                 date:     p.at('time')['datetime'],
-                 content:  content.inner_html
-               }
-             }
+def parse_forum(id)
+  doc = download_forum(id) rescue nil
+  return if doc.nil? # forum does not exist
+  atts = {
+    id: id,
+    name: doc.at('h2').content.strip rescue "",
+    description: doc.at('p[class="forum-description cl-af"]').content rescue "",
+    parent: doc.search('meta[itemprop="position"]').last.parent['data-forum-id'].to_i rescue 0 # 0 if root
+  }
 end
